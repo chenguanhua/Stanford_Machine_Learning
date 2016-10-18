f15c5c0cb2b583abc8ee71a8e3faaee9f68a9a1e
chenguanhua <ghchen5d2012@gmail.com>
2016-10-18 15:53:36 -0400

    w7 finished

--- w2/ex1/ex1_multi.m
+++ w2/ex1/ex1_multi.m
@@ -122,7 +122,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = [ones(1, 1) ([1650 3]-mu)./sigma]*theta; % You should change this
 
 
 % ============================================================
@@ -167,7 +167,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1, 1650, 3]*theta; % You should change this
 
 
 % ============================================================
